8c28fbda854d79ad57affd258db2dec7edcef785
Johnny-Oleg <neojin.entity@gmail.com>
2022-04-09 19:26:37 +0300

    contacts section

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const $menuRight = document.querySelector('.menu-list__right');
 const $menuLeft = document.querySelector('.menu-list__left');
 const $textRight = document.querySelector('.inner-item__title--right');
 const $textLeft = document.querySelector('.header__inner-item');
+const $form = document.querySelector('.form');
 
 function time() {
 	setTimeout(() => {
@@ -13,3 +14,33 @@ function time() {
 }
 
 window.addEventListener('load', time);
+
+// document.addEventListener('scroll', () => {
+// 	const $html = document.documentElement;
+//     $html.dataset.scroll = window.scrollY;
+
+// 	   $html.dataset.scroll >= 1000 ? $form.classList.add('slide'): '';
+// })
+
+const observer = new IntersectionObserver(function(elems, observer) {
+	elems.forEach(elem => {
+		if (!elem.isIntersecting) return;
+		
+		elem.target.classList.add('slide');
+        console.log(elem.target);
+		observer.unobserve(elem.target);
+	})
+}, {
+	root: null,
+	threshold: 0,
+	rootMargin: ''
+})
+
+observer.observe($form);
+
+$form.addEventListener('submit', e => {
+	e.preventDefault();
+
+	const $valid = document.querySelector('.valid');
+	$valid.style.display = 'block';
+})
